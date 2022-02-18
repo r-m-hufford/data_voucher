a46f966ada0c1a39d87221fd0b05b8e44cbb765f
Ryan Hufford <ryan@rmhufford.com>
2022-02-18 09:13:42 -0500

    starting to tinker with inline documentation

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const isPassword = require('./password')
 
+/**
+ * 
+ * @param {*} string
+ * this function does some cool stuff that you will need to do
+ * if you want to do cool stuff
+ * @returns boolean
+ */
 function isPhoneNumber(string) {
   return string === 'phone number';
 }
